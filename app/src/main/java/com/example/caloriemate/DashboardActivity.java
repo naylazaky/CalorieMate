7a3e865c1ff753f5bd23edb5412a1a4a1a88ba7f
naylazaky <naylaazakyfauziah@gmail.com>
2026-06-06 20:52:46 +0800

    feat: add Dashboard, Navigation, and empty Fragments

---
+++ app/src/main/java/com/example/caloriemate/DashboardActivity.java
@@ -0,0 +1,26 @@
+package com.example.caloriemate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.navHostFragment);
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+}
